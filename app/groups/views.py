6ca24fcf12328bffb6d6a4154363affb103e81a3
Anastasiya <maslastja@gmail.com>
2019-10-11 11:18:16 +0800

    change register bp

--- app/groups/views.py
+++ app/groups/views.py
@@ -3,7 +3,8 @@ from app.func import gr_select, stud_select, login_required
 from app.groups.func import (find_all_groups, add_group, 
                           update_group, delete_group)
 from app.groups.forms import AddGroupForm, UpGroupForm, DelGroupForm
-
+from secretary import Renderer
+import subprocess
 
 #@bp.route('/addgroup', methods=['GET', 'POST'])
 @login_required
@@ -65,4 +66,23 @@ def delgroup():
     else:    
         return render_template('delgroup.html', form=form,  
                                title='Удалить группу')
-
+@login_required
+def test_secr():
+    engine = Renderer()
+    template = open('app/groups/template.odt', 'rb')
+    output = open('output.odt', 'wb')
+    eng = engine.render(template, image='app/groups/writer.png')
+    output.write(eng)
+    p = subprocess.call(['unoconv', '-f', 'pdf', 'output.odt'])
+    with open('output.pdf', 'rb') as f:
+        file_content = f.read()
+    
+    response = make_response(file_content, 200)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = \
+                'inline'
+    #response.headers['Content-Disposition'] = \
+                #'inline; filename=output.pdf'
+    return response    
+     #return ("Template rendering finished! Check output.odt file.")
+    
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-#!/home/Maslastja/Документы/project1/.venv/env/bin/python3
 from app.core import create_app
 
 app = create_app()
