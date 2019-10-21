0ce8a60e9ac04283c9ae0462fdac7438e27dc5e8
Anastasiya <maslastja@gmail.com>
2019-10-21 10:37:58 +0800

    add console com when debug true

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
+import os
 from app.core import create_app
-from app.commands import dbase
 
 app = create_app()
-app.cli.add_command(dbase)
+if os.getenv('DEBUG')=='True':
+    from app.commands import dbase
+    #print(os.getenv('DEBUG'))
+    app.cli.add_command(dbase)
 
 #app.run('localhost', 8080)
