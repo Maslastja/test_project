b1c5c13fe0503bfee5434cb5d5962ff41345885f
Anastasiya <maslastja@gmail.com>
2019-10-25 13:18:32 +0800

    rework main form

--- app/groups/__init__.py
+++ app/groups/__init__.py
@@ -4,9 +4,7 @@ from . import views
 bp = Blueprint('groups', __name__, template_folder='templates')
 options = {'url_prefix': '/group'}
 
-bp.add_url_rule('/group', view_func=views.groupform, 
-                methods=['GET', 'POST'])
+bp.add_url_rule('/group', view_func=views.groupform, methods=['GET', 'POST'])
 bp.add_url_rule('/list', view_func=views.get_groups, methods=['GET', 'POST'])
-bp.add_url_rule('/del', view_func=views.delgroup, methods=['GET', 'POST'])
 
 bp.add_url_rule('/test', view_func=views.test_secr)
--- app/students/__init__.py
+++ app/students/__init__.py
@@ -4,9 +4,6 @@ from . import views
 bp = Blueprint('students', __name__, template_folder='templates')
 options = {'url_prefix': '/student'}
 
-bp.add_url_rule('/student', view_func=views.studentform, 
-                methods=['GET', 'POST'])
+bp.add_url_rule('/student', view_func=views.studentform, methods=['GET', 'POST'])
 bp.add_url_rule('/list', view_func=views.get_students, methods=['GET', 'POST'])
-bp.add_url_rule('/del', view_func=views.delstudent, 
-                methods=['GET', 'POST'])
 
