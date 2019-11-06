89181a4b3b0289bcafe2733293793afa8aba169a
Anastasiya <maslastja@gmail.com>
2019-11-06 10:44:21 +0800

    utils.py

--- app/utils.py
+++ app/utils.py
@@ -8,15 +8,16 @@ from functools import wraps
 def logapp(app):
     if not os.path.exists('logs'):
         os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/app.log', 
-                                            maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: '
-        '%(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('test app')
+    
+    file_handler = RotatingFileHandler('logs/app.log', 
+                                        maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: '
+    '%(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('test app')
 
 def register_bp(app):
     #print(app.import_name)
